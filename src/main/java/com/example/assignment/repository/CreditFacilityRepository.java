9ae9932f23e860f250554f0b585d2cd3df1fc75c
Edric Nguyen <edric.nguyen@sunseries.travel>
2023-01-11 00:03:39 +0700

    update validate

--- src/main/java/com/example/assignment/repository/CreditFacilityRepository.java
+++ src/main/java/com/example/assignment/repository/CreditFacilityRepository.java
@@ -4,4 +4,5 @@ import com.example.assignment.domain.entity.CreditFacility;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CreditFacilityRepository extends JpaRepository<CreditFacility, Long> {
+    CreditFacility findByApplicant(String applicant);
 }
--- src/main/java/com/example/assignment/service/impl/LoanServiceImpl.java
+++ src/main/java/com/example/assignment/service/impl/LoanServiceImpl.java
@@ -2,19 +2,24 @@ package com.example.assignment.service.impl;
 
 import com.example.assignment.domain.dto.LoanDTO;
 import com.example.assignment.domain.dto.PaymentDTO;
+import com.example.assignment.domain.entity.CreditFacility;
 import com.example.assignment.domain.entity.Loan;
 import com.example.assignment.domain.entity.Payment;
 import com.example.assignment.domain.mapper.LoanMapper;
 import com.example.assignment.domain.mapper.PaymentMapper;
+import com.example.assignment.repository.CreditFacilityRepository;
 import com.example.assignment.repository.LoanRepository;
 import com.example.assignment.repository.PaymentRepository;
 import com.example.assignment.service.CreditFacilityService;
 import com.example.assignment.service.LoanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,7 +27,7 @@ public class LoanServiceImpl implements LoanService {
 
     private final LoanRepository loanRepository;
 
-    private final CreditFacilityService creditFacilityService;
+    private final CreditFacilityRepository creditFacilityRepository;
     private final LoanMapper loanMapper;
 
     private final PaymentMapper paymentMapper;
@@ -31,10 +36,28 @@ public class LoanServiceImpl implements LoanService {
     public Loan save(LoanDTO loanDTO) {
 
         //TODO: validate loan data
+        Assert.isTrue(loanDTO.getLoanId() != null, "loanId is required");
+        Assert.isTrue(loanDTO.getApplicant() != null, "applicant is required");
+        Assert.isTrue(loanDTO.getCurrency() != null, "currency is required");
+        Assert.isTrue(loanDTO.getStartDate() != null, "startDate is required");
+        Assert.isTrue(loanDTO.getEndDate() != null, "endDate is required");
+        Assert.isTrue(loanDTO.getAmount() != null, "amount is required");
+        Assert.isTrue(loanDTO.getInterestRate() != null, "rate is required");
+
 
         //TODO: validate limit of credis of applicant: total loan amount < credit
 
+        CreditFacility credit = creditFacilityRepository.findByApplicant(loanDTO.getApplicant());
+
+        List<Loan> loansOfApplicant = getLoanByApplicant(loanDTO.getApplicant());
+
+        Double totalDebt = loansOfApplicant.stream().map(Loan::getAmount).collect(Collectors.summingDouble(Double::intValue));
+
+        Assert.isTrue(credit.getTotalLimit() <= totalDebt, "cant create loan. total debt is maximum");
+
         //TODO: validate startDate < now < endDate of credit
+
+        Assert.isTrue(credit.getStartDate().isBefore(LocalDate.now()) && credit.getEndDate().isAfter(LocalDate.now()), "credit is out of date");
 
         return loanRepository.save(loanMapper.toEntity(loanDTO));
     }
